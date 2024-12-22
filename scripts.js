0a09ebfdd80bd779a984a7f815a82cc457603f49
seif8545 <seifomaraly123@gmail.com>
2024-12-22 13:07:39 +0200

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,20 @@
+function showPage1() {
+    document.getElementById('content').innerHTML = `
+        <h2>This is Page 1</h2>
+        <p>Welcome to the first page of my interactive web page!</p>
+    `;
+}
+
+function showPage2() {
+    document.getElementById('content').innerHTML = `
+        <h2>This is Page 2</h2>
+        <p>Welcome to the second page of my interactive web page!</p>
+    `;
+}
+
+function showPage3() {
+    document.getElementById('content').innerHTML = `
+        <h2>This is Page 3</h2>
+        <p>Welcome to the third page of my interactive web page!</p>
+    `;
+}
